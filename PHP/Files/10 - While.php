84c5351a07638f5ccdf7b3993077d159a1fa5b7f
Pedro Kielblock <p.kielblock123@gmail.com>
2021-09-25 17:01:03 -0300

    New Files

---
+++ PHP/Files/10 - While.php
@@ -0,0 +1,10 @@
+<?php
+
+$x = 10;
+
+echo "Contador" . PHP_EOL;
+
+while ($x < 20){
+    $x++;
+    echo "O Valor de X: " . $x . PHP_EOL;
+}
---
+++ PHP/Files/11 - DoWhile.php
@@ -0,0 +1,8 @@
+<?php
+
+$conta = 0;
+
+do {
+    $conta++;
+    echo $conta . PHP_EOL;
+}while($conta < 10);
